f118a84f13b97063ad7ac6855bb7e7ed91e97edb
Lalan Kumar <99739282+kumar8074@users.noreply.github.com>
2025-07-14 20:35:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,11 @@ load_dotenv()
 # API keys
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
-COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 GEMINI_API_KEY=os.getenv("GOOGLE_API_KEY")
 
 # Model settings
-LLM_MODEL = "gemini-2.0-flash"
+LLM_MODEL = "gemini-2.5-flash"
 
 
 # Get current date
